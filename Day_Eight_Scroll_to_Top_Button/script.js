bd41f4f51a1f5e530eee1e4584e6944a5862cb96
ARShishir <shishir01022003@gmail.com>
2025-10-06 00:23:01 +0600

    Day Eight Done
    Day Eight Scroll to Top Button

---
+++ Day_Eight_Scroll_to_Top_Button/script.js
@@ -0,0 +1,14 @@
+ const scrollBtn = document.getElementById("scrollTopBtn");
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 300) {
+        scrollBtn.classList.add("show");
+      } else {
+        scrollBtn.classList.remove("show");
+      }
+    });
+    //  Author : Abdur Rahaman Shishir 
+
+    scrollBtn.addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
